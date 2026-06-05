a31b9b8db4c163eff12a0cde032802645062d88b
mukesh <mitu4846t@gmail.com>
2026-06-05 20:40:56 +0530

    done

--- frontend/frontend/src/components/loginform.jsx
+++ frontend/frontend/src/components/loginform.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { loginUser } from "../services/authService";
+import { loginUser } from "../services/authservice";
 import { useAuth } from "../services/authcontext";
 
 
